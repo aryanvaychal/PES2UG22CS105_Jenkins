c35b114badb40ffeb05baa49f0e981640877c18d
aryanvaychal <aryanvaychal999@gmail.com>
2025-03-10 18:48:52 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS105 has successfully built and run this  \n";
+  cout << "PES2UG22CS105 has successfully executed webhooks  \n";
   return 0;
 }
