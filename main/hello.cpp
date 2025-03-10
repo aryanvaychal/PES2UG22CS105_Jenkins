e30afe09c3938046a84dede47638ab8bbda4039a
aryanvaychal <aryanvaychal999@gmail.com>
2025-03-10 16:33:33 +0530

    Describe this commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG22CS123 has successfully built and run this  \n";
+  return 0;
+}
